1c04d6b4c013e69ad98044e7e372502cc85658d4
spydaz <LeroySamuelDyer@hotmail.co.uk>
2023-07-19 18:36:37 +0300

    Updates

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.SpydazWebAI_PositionalEncoderDecoder.My.MySettings
+        Friend ReadOnly Property Settings() As Global.SpydazWebAI.PositionalEncoderDecoder.My.MySettings
             Get
-                Return Global.SpydazWebAI_PositionalEncoderDecoder.My.MySettings.Default
+                Return Global.SpydazWebAI.PositionalEncoderDecoder.My.MySettings.Default
             End Get
         End Property
     End Module
